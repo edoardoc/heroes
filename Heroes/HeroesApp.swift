ffb6fd2f6f31c0ab843881516600028bc53f3766
Edoardo Ceccarelli <ridleys@gmail.com>
2021-07-18 18:33:15 +0300

    initial commit

---
+++ Heroes/HeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroesApp.swift
+//  Heroes
+//
+//  Created by Edoardo on 18/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct HeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
